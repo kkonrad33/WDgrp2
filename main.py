985d0ee3ccefe6e40a460932c01bcd9c1fb3080f
kkonrad33 <73562898+kkonrad33@users.noreply.github.com>
2021-05-12 21:06:49 +0200

    matplotlib wprowadzenie zadania

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import xlrd
-# Stwórz wykres liniowy, który wyświetli liczbę urodzonych dzieci dla każdego roku.
 
-# xlsx = pd.ExcelFile('imiona.xlsx')
-# df = pd.read_excel(xlsx, header=0)
+# zadanie 1 i 2
 
-# grupa = df.groupby(['Rok']).agg({'Liczba':['sum']})
-# wykres = grupa.plot()
-# wykres.set_ylabel('liczba dzieci')
-# wykres.set_xlabel('rok')
-# wykres.legend()
-# plt.title('Liczba urodzonych dzieci w każdym roku')
+# x = np.arange(1,21,1)
+# plt.plot(x, 1/x, 'g>:', label='f(x) = 1/x')
+# plt.xlabel('x')
+# plt.ylabel('f(x)')
+# plt.axis([0,20,0,1])
+# plt.title('Wykres funkcji f(x) dla x[1,20]')
+# plt.legend()
 # plt.show()
 
-# Stwórz wykres słupkowy, który wyświetli liczbę urodzonych chłopców i dziewczynek z całego zbioru.
+# zadanie 3
 
-# grupa = df.groupby(['Plec']).agg({'Liczba':['sum']})
-# wykres = grupa.plot.bar()
-# wykres.set_ylabel('liczba dzieci')
-# wykres.set_xlabel('Płeć')
-# wykres.legend()
-# plt.title('Ilość urodzonych dzieci z podziałem na płeć')
+# x1 = np.arange(0.0,30.0,0.1)
+# x2 = np.arange(0.0,30.0,0.1)
+# y1 = np.sin(x1)
+# y2 = np.cos(x2)
+
+# plt.subplot(2, 1, 1)
+# plt.plot(x1, y1, 'c-', label=('sin(x)'))
+# plt.ylabel('sin(x)')
+# plt.xlabel('x')
+# plt.legend()
+
+# plt.subplot(2, 1, 2)
+# plt.plot(x2, y2, 'm-', label=('cos(x)'))
+# plt.ylabel('cos(x)')
+# plt.xlabel('x')
+# plt.legend()
 # plt.show()
 
-# Wykres kołowy z wartościami % ukazującymi ilość urodzonych chłopców i dziewczynek w ostatnich 5 latach z datasetu.
+# zadanie 4
+
+# x1 = np.arange(0.0,30.0,0.1)
+# x2 = np.arange(0.0,30.0,0.1)
+# y1 = 2 + np.sin(x1)
+# y2 = -1 * np.sin(x2)
 #
-# ndf = (df[((df.Rok >= 2012) & (df.Rok <= 2017))])
-#
-# grupa = ndf.groupby(['Plec']).agg({'Liczba':['sum']})
-# wykres = grupa.plot.pie(subplots=True, autopct='%.2f %%', fontsize=16, figsize=(5,5), legend=(0,0))
-# plt.legend(loc="upper right")
-# plt.title('Ilość urodzonych dzieci z podziałem na płeć')
+# plt.plot(x1, y1, '-', label=('sin(x)'))
+# plt.ylabel('sin(x)')
+# plt.xlabel('x')
+# plt.plot(x2, y2, label=('sin(x)'))
+# plt.ylabel('sin(x)')
+# plt.xlabel('x')
+# plt.legend(loc='center left')
+# plt.title('Wykres sin(x), sin(x)')
 # plt.show()
+
